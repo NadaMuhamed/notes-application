88d03e88d9619383cea5da1e1f940aa917f651c2
Nada Mohamed <nadamohamed1620019@gmail.com>
2026-05-05 13:26:03 +0300

    Preparing for deploying

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,13 +3,17 @@ const connectDB = require('./config/db');
 require('dotenv').config();
 const rateLimiter = require('./middleware/rateLimiter');
 const cors = require('cors');
+const path = require('path');
 const app = express();
-app.use(cors(
-    {
-        origin: 'http://localhost:5173',
 
-    }
-));
+if(process.env.NODE_ENV === 'development') {
+    app.use(cors(
+        {
+            origin: 'http://localhost:5173',
+    
+        }
+    ));
+}
 app.use(express.json());
 app.use(rateLimiter);
 
@@ -18,7 +22,13 @@ connectDB();
 const notesRoutes = require('./routes/notesRoutes');
 app.use('/api/notes', notesRoutes);
 
-
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../../frontend/dist')));
+    
+    app.get(/.*/, (req, res) => {
+        res.sendFile(path.join(__dirname, '../../frontend/dist/index.html'));
+    });
+}
 
 
 
